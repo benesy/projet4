9136c34a844161c3116aed41790bcf9902eca57d
Charlotte Gerard <charlotte.julie.gerard@gmail.com>
2020-01-30 15:14:00 +0100

    insertion en bdd

--- src/Projet4ConsoleTest/Program.cs
+++ src/Projet4ConsoleTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projet4DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,32 @@ namespace Projet4ConsoleTest
     {
         static void Main(string[] args)
         {
+            try {
+                f1();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Tu sais pas coder ! : {0}", e);
+            }
+
         }
+
+        static void f1()
+        {
+            projet4DAO ctx = new projet4DAO();
+
+            captor c = new captor();
+            c.serial_number = "123457";
+            c.localisation = "hangar b";
+            c.description = "botte de radis";
+
+            ctx.captor.Add(c);
+            ctx.SaveChanges();
+
+           
+
+
+        }
+
     }
 }
